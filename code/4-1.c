826d23df8c551ead5be2ea325ffed2ec7126dc1a
qkrtlgus0225-coder <qkrtlgus0225@gmail.com>
2026-03-18 16:58:56 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -2,7 +2,24 @@
 
 int main()
 {
-
+ int num1;
+    int num2;
+    char op;
+    scanf("%d %d %c", &num1, &num2, &op);
+    switch (op){
+        case '+':
+            printf("%d",num1+num2);
+            break;
+        case '-':
+            printf("%d",num1-num2);
+            break;
+        case '*':
+            printf("%d",num1*num2);
+            break;
+        case '/':
+            printf("%d",num1/num2);
+            break;
+    }
     return 0;
 }
 
