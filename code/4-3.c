39caa2b48f46875ef1843561a866a12e1bf8d37a
qkrtlgus0225-coder <qkrtlgus0225@gmail.com>
2026-03-18 16:59:42 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,7 +2,21 @@
 
 int main()
 {
-
+  int num;
+    bool flag = true;
+    scanf("%d",&num);
+    for (int i=2;i<num;i++){
+        if (num%i==0){
+            flag = false;
+            break;
+        }
+    }
+    if (flag&&num>1){
+        printf("true");
+    }
+    else{
+        printf("false");
+    }
     return 0;
 }
 
