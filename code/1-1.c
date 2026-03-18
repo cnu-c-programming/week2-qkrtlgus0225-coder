27cf9e543c8e9fd0bca4df32c27272a6b3deab4f
qkrtlgus0225-coder <qkrtlgus0225@gmail.com>
2026-03-18 16:46:45 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
 int main()
-{
+{ printf("int : %d\n", var1);
+    printf("hex : %x\n", var1);
+    printf("oct : %o\n", var1);
+
+    char var2 = 'C';
+    printf("char : %c\n", var2);
+
+    char* var3 = "hello world!";
+    printf("string : %s\n", var3);
+
+    float var4 = 3.141592;
+    printf("float : %f\n", var4);
+    printf("exp : %e\n", var4);
 
     return 0;
 }
