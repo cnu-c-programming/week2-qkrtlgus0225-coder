399044f288d482eed0f27b00d8d3117ad22fbadd
qkrtlgus0225-coder <qkrtlgus0225@gmail.com>
2026-03-19 12:53:09 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,25 +2,31 @@
 
 int main()
 {
-   int num;
-    bool flag = true;
-    scanf("%d",&num);
-    if (num < 2) {
-        flag = false; 
-    } else {
-        for (int i = 2; i * i <= num; i++) { 
-            if (num % i == 0) {
-                flag = false;
+    int n, i;
+    int isPrime = 1;
+
+    scanf("%d", &n);
+
+    if (n <= 1)
+    {
+        isPrime = 0;
+    }
+    else
+    {
+        for (i = 2; i < n; i++)
+        {
+            if (n % i == 0)
+            {
+                isPrime = 0;
                 break;
             }
         }
     }
-    
-    if (flag) {
-        printf("true");
-    } else {
-        printf("false");
-    }
+
+    if (isPrime)
+        printf("true\n");
+    else
+        printf("false\n");
     return 0;
 }
 
